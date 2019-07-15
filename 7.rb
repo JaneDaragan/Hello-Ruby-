06d893a8dc3413625463f580bd5f98fcd40bc443
JaneDaragan <51912999+JaneDaragan@users.noreply.github.com>
2019-07-15 08:54:18 +0600

    Add files via upload

--- 7.rb
+++ 7.rb
@@ -3,14 +3,13 @@ def score_throws(radiuses)
   return 0 if radiuses.empty?
   array=[]
   radiuses.each do |r|
-    radiuses = r.to_f
-    array << 0 if radiuses > 10
-    array << 10 if radiuses < 5
-    array << 5 if radiuses >= 5 && radiuses <= 10
+    array << 0 if r > 10
+    array << 10 if r < 5
+    array << 5 if r >= 5 && r <= 10
   end
 
   score = array.sum
-  score += 100 if radiuses.all? {|r| r < 5}
+  score += 100 if radiuses.all? {|r| r <= 5}
 
   puts "Score: #{score}"
 end
